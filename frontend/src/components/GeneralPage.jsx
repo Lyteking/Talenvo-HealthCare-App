1a2671c0722248e85176711e2693489b93212fed
lyteking <lightebube751@gmail.com>
2025-03-05 08:52:51 +0100

    Updated

--- frontend/src/components/GeneralPage.jsx
+++ frontend/src/components/GeneralPage.jsx
@@ -1,11 +1,46 @@
-import React from "react";
+import React, {useState} from "react";
 import '../index.css'
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import HomePage from './HomePage'
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 
 export default function GeneralPage(){
+    const [page, setPage] = useState(<HomePage/>)
+    let home = true
+    let explore = false
+    let bookings = false
+    let saved = false
+    let profile = false
+
+    function handleHome(){
+        if (home){
+            explore = false
+            bookings = false
+            saved = false
+            profile = false
+            setPage('./home-page')
+        }
+    }
+    function handleExplore(){
+        if (explore){
+            home = false
+            bookings = false
+            saved = false
+            profile = false
+            setPage('./explore')
+        }
+    }
+    function handleBooking(){
+        if (bookings){
+            explore = false
+            bookings = false
+            saved = false
+            profile = false
+            setPage('./booking')
+        }
+    }
     return(
         <>
-            <header className="shadow-md flex justify-between p-6">
+            <header className="fixed w-full z-50 bg-white shadow-md flex justify-between p-6">
                 <h1>TALENVO</h1>
                 <section className="hidden md:flex md:justify-around gap-6 items-end">
                 <section className="bg-noti rounded-2xl p-1"><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
@@ -17,46 +52,50 @@ export default function GeneralPage(){
 </section>
 </section>
             </header>
-            <section className="md:h-4/5 md:flex-col md:justify-between md:flex">
-    <section className="md:h-91/100 md:fixed md:top-20 md:left-0 md:flex md:flex-col md:ml-0 shadow-lg">
-        {/* Navigation Items */}
-        <section className="flex flex-col gap-4">
-            <section className="nav-btn">
-                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
-                    <path strokeLinecap="round" strokeLinejoin="round" d="m2.25 12 8.954-8.955c.44-.439 1.152-.439 1.591 0L21.75 12M4.5 9.75v10.125c0 .621.504 1.125 1.125 1.125H9.75v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21h4.125c.621 0 1.125-.504 1.125-1.125V9.75M8.25 21h8.25" />
-                </svg>
-                <p>Home</p>
-            </section>
+            <main className="md:ml-20">
+                {page}
+                {page}
+            </main>
+            <section className="flex flex-row md:h-4/5 md:flex-col md:justify-between md:flex">
+            <section className="flex border-t-1 border-gray-300 pt-2 flex-row justify-between fixed bottom-0 md:w-20 md:h-91/100 md:fixed md:top-20 md:left-0 md:flex md:flex-col md:ml-0 w-full bg-white shadow-lg"> {/* Background color added along with shadow */}
+            {/* Navigation Items */}
+    <section className="flex justify-around flex-row w-full  md:flex-col gap-4">
+      <section className="nav-btn">
+        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
+          <path strokeLinecap="round" strokeLinejoin="round" d="m2.25 12 8.954-8.955c.44-.439 1.152-.439 1.591 0L21.75 12M4.5 9.75v10.125c0 .621.504 1.125 1.125 1.125H9.75v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21h4.125c.621 0 1.125-.504 1.125-1.125V9.75M8.25 21h8.25" />
+        </svg>
+        <p>Home</p>
+      </section>
 
-            <section className="nav-btn">
-                <svg width="26" height="26" viewBox="0 0 26 26" fill="none" xmlns="http://www.w3.org/2000/svg">
-                    <path d="M13 11.8083C12.3391 11.8083 11.8083 12.3392 11.8083 13C11.8083 13.6608 12.3391 14.1917 13 14.1917C13.6608 14.1917 14.1916 13.6608 14.1916 13C14.1916 12.3392 13.6608 11.8083 13 11.8083ZM13 2.16666C7.01996 2.16666 2.16663 7.01999 2.16663 13C2.16663 18.98 7.01996 23.8333 13 23.8333C18.98 23.8333 23.8333 18.98 23.8333 13C23.8333 7.01999 18.98 2.16666 13 2.16666ZM15.3725 15.3725L6.49996 19.5L10.6275 10.6275L19.5 6.49999L15.3725 15.3725Z" fill="#0077CC"/>
-                </svg>
-                <p>Explore</p>
-            </section>
+      <section className="nav-btn">
+        <svg width="26" height="26" viewBox="0 0 26 26" fill="none" xmlns="http://www.w3.org/2000/svg">
+          <path d="M13 11.8083C12.3391 11.8083 11.8083 12.3392 11.8083 13C11.8083 13.6608 12.3391 14.1917 13 14.1917C13.6608 14.1917 14.1916 13.6608 14.1916 13C14.1916 12.3392 13.6608 11.8083 13 11.8083ZM13 2.16666C7.01996 2.16666 2.16663 7.01999 2.16663 13C2.16663 18.98 7.01996 23.8333 13 23.8333C18.98 23.8333 23.8333 18.98 23.8333 13C23.8333 7.01999 18.98 2.16666 13 2.16666ZM15.3725 15.3725L6.49996 19.5L10.6275 10.6275L19.5 6.49999L15.3725 15.3725Z" fill="#0077CC"/>
+        </svg>
+        <p>Explore</p>
+      </section>
 
-            <section className="nav-btn">
-                <svg width="26" height="26" viewBox="0 0 26 26" fill="none" xmlns="http://www.w3.org/2000/svg">
-                    <path d="M24.375 2.43466L17.8693 2.43468V0.815369C17.8693 0.366462 17.5057 0.00286865 17.0568 0.00286865C16.6079 0.00286865 16.2443 0.366462 16.2443 0.815369V2.43427H9.74431V0.815369C9.74431 0.366462 9.38072 0.00286865 8.93181 0.00286865C8.48291 0.00286865 8.11931 0.366462 8.11931 0.815369V2.43427H1.625C0.727594 2.43427 0 3.16187 0 4.05927V24.3718C0 25.2692 0.727594 25.9968 1.625 25.9968H24.375C25.2724 25.9968 26 25.2692 26 24.3718V4.05927C26 3.16225 25.2724 2.43466 24.375 2.43466Z" fill="#333333"/>
-                </svg>
-                <p>Bookings</p>
-            </section>
+      <section className="nav-btn">
+        <svg width="26" height="26" viewBox="0 0 26 26" fill="none" xmlns="http://www.w3.org/2000/svg">
+          <path d="M24.375 2.43466L17.8693 2.43468V0.815369C17.8693 0.366462 17.5057 0.00286865 17.0568 0.00286865C16.6079 0.00286865 16.2443 0.366462 16.2443 0.815369V2.43427H9.74431V0.815369C9.74431 0.366462 9.38072 0.00286865 8.93181 0.00286865C8.48291 0.00286865 8.11931 0.366462 8.11931 0.815369V2.43427H1.625C0.727594 2.43427 0 3.16187 0 4.05927V24.3718C0 25.2692 0.727594 25.9968 1.625 25.9968H24.375C25.2724 25.9968 26 25.2692 26 24.3718V4.05927C26 3.16225 25.2724 2.43466 24.375 2.43466Z" fill="#333333"/>
+        </svg>
+        <p>Bookings</p>
+      </section>
 
-            <section className="nav-btn">
-                <svg width="26" height="26" viewBox="0 0 26 26" fill="none" xmlns="http://www.w3.org/2000/svg">
-                    <path d="M4.33337 5.41667V21.2182C4.33337 22.1823 5.50012 22.6666 6.18262 21.9841L13 15.1667L19.8175 21.9841C20.5 22.6666 21.6667 22.1834 21.6667 21.2182V5.41667C21.6667 4.84203 21.4384 4.29093 21.0321 3.8846C20.6258 3.47827 20.0747 3.25 19.5 3.25H6.50004C5.9254 3.25 5.3743 3.47827 4.96798 3.8846C4.56165 4.29093 4.33337 4.84203 4.33337 5.41667Z" stroke="#333333" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
-                </svg>
-                <p>Saved</p>
-            </section>
-        </section>
-
-        {/* Profile SVG Positioned at the Bottom */}
-        <section className="mt-auto flex justify-center py-4">
-            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
-                <path strokeLinecap="round" strokeLinejoin="round" d="M17.982 18.725A7.488 7.488 0 0 0 12 15.75a7.488 7.488 0 0 0-5.982 2.975m11.963 0a9 9 0 1 0-11.963 0m11.963 0A8.966 8.966 0 0 1 12 21a8.966 8.966 0 0 1-5.982-2.275M15 9.75a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" />
-            </svg>
-        </section>
+      <section className="nav-btn">
+        <svg width="26" height="26" viewBox="0 0 26 26" fill="none" xmlns="http://www.w3.org/2000/svg">
+          <path d="M4.33337 5.41667V21.2182C4.33337 22.1823 5.50012 22.6666 6.18262 21.9841L13 15.1667L19.8175 21.9841C20.5 22.6666 21.6667 22.1834 21.6667 21.2182V5.41667C21.6667 4.84203 21.4384 4.29093 21.0321 3.8846C20.6258 3.47827 20.0747 3.25 19.5 3.25H6.50004C5.9254 3.25 5.3743 3.47827 4.96798 3.8846C4.56165 4.29093 4.33337 4.84203 4.33337 5.41667Z" stroke="#333333" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
+        </svg>
+        <p>Saved</p>
+      </section>
     </section>
+
+    {/* This section will be visible only on medium and larger screens */}
+    <section className="hidden md:mt-auto md:flex md:justify-center md:py-4">
+      <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
+        <path strokeLinecap="round" strokeLinejoin="round" d="M17.982 18.725A7.488 7.488 0 0 0 12 15.75a7.488 7.488 0 0 0-5.982 2.975m11.963 0a9 9 0 1 0-11.963 0m11.963 0A8.966 8.966 0 0 1 12 21a8.966 8.966 0 0 1-5.982-2.275M15 9.75a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" />
+      </svg>
+    </section>
+  </section>
 </section>
 
 
--- frontend/src/components/HomePage.jsx
+++ frontend/src/components/HomePage.jsx
@@ -1,40 +1,42 @@
 import React from "react";
 import '../index.css'
-import image from '../assets/image.png'
+import image from '../assets/doctor.png'
 import image2 from '../assets/image2.png'
 
 export default function Test(){
     return(
         <main className='py-4'>
-        <section className='relative'>
-        <section className='font-bold flex grid-cols-3 pt-20 pb-16 -mb-8 px-2 mx-1 md:flex md:grid-cols-2'>
-            <section className='col-span-2 max-h-1/2 md:col-span-1'>
+        <section className="md:relative"> 
+        <section className='relative md:h-full font-bold flex grid-cols-3 pt-20 pb-16 -mb-8 px-2 mx-1 md:flex md:grid-cols-2'>
+            <section className='md:pt-16 col-span-2 md:pl-5 max-h-2/3 md:col-span-1'>
                 <h1 className='text-lg md:text-big'>Your Path to</h1>
                 <h1 className='text-lg text-blue-300 md:text-big'>Affordable Healthcare</h1>
-                <h1 className='text-lg'>Starts Here</h1>
-                <p className='text-para h-2.5 md:text-lg md:w-3/5 '>Easily locate providers, view services and schedule your next visit with confidence</p>
+                <h1 className='text-lg md:text-big'>Starts Here</h1>
+                <p className='text-para h-2.5 md:text-lg md:w-full '>Easily locate providers, view services and schedule your next visit with confidence</p>
             </section>
-            <img className='w-26 h-50 col-span-1 md:w-40 md:h-50 md:col-span-1' src={image} alt='doctor'/>
+            <img className='w-26 h-50 col-span-1 md:w-2/5 md:h-full md:col-span-1' src={image} alt='doctor'/>
         </section>
-        <section className='bg-gray-200 px-2 py-8 rounded-2xl mx-6 absolute top-7/10'>
-            <section className='flex justify-between'>
+        <section className="absolute md:top-7/10 md:w-full flex justify-center " >
+        <section className='md:absolute md:top-5 md:w-3/5 md:h-2/10 flex md:justify-center md:items-center md:transform md:-translate-y-1/2 md:z-10 bg-gray-200 md:p-4 rounded-md shadow-lg'>
+            <section className='flex absolute justify-between'>
                 <input placeholder='Search by Name of Hospital' type='text' className='bg-white p-2 w-11/20 text-sm'/>
                 <input placeholder='Enter Location' type='text' className='bg-white p-2 w-2/5 text-sm'/>
             </section>
             </section>
-        </section>
-        <section className='bg-blue-400 p-6 md:flex md:justify-center '>
-        <section className='p-8'>
-            <img src={image2} alt='image' className='w-50 text-center h-30 rounded-2xl'/>
+        <section className='bg-blue-400 p-4 md:flex md:justify-between md:pt-20 md:pb-10'>
+        <section className='w-1/2'>
+            <img src={image2} alt='image' className='object-cover w-full h-full text-center h-30 rounded-2xl'/>
         </section>
         <section className="md:flex md:flex-col md:mt-4">
-        <p className='text-blue-800 text-center text-xl font-bold'>Why Choose Our Platform?</p>
-        <ul className='list-disc'>
-            <li className='mt-2'>Lorem ipsum</li>
-            <li className='mt-2'>Lorem ipsum</li>
-            <li className='mt-2'>Lorem ipsum</li>
-            <li className='mt-2'>Lorem ipsum</li>
+        <p className='text-blue-800 text-center text-xl md:text-3xl font-bold'>Why Choose Our Platform?</p>
+        <ul className='list-disc md:ml-10 md:text-lg md:font-bold'>
+            <li className='mt-2'>Locate trusted doctors, clinics, and hospitals in your area</li>
+            <li className='mt-2'>Compare consultation fees and check accepted insurance plans.</li>
+            <li className='mt-2'>Book appointments with reminders to ensure you never miss a visit.</li>
+            <li className='mt-2'>Help improve healthcare by sharing your feedback.m</li>
         </ul>
+        </section>
+        </section>
         </section>
         </section>
     </main>
